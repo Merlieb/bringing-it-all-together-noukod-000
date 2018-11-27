48f75958007848095db3b86a92645fa783a20813
Merline Belizaire <merline@noukod.com>
2018-11-27 19:20:34 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -26,7 +26,7 @@ class Dog
 
   def update
     sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
-    DB[:conn].execute(sql, self.name, self.breed, self.id)
+    DB[:conn].execute(sql, self.id, self.name, self.breed)
   end
 
   def save
